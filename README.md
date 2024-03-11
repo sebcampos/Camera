5dbc73eafb8679712a4e4d258fd819536ace8332
sebash <sebcampos@criptext.com>
2024-03-10 19:21:35 -0700

    test2 works, thank you internet

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Resources
 - [YouTube tutorial](https://www.youtube.com/playlist?list=PLYV_j9XEhvorTV-ClcNA2xUb5YsdUHgRX) by youtuber `Edge AI Guy` teaches how to build tensorflow from source on different platforms including macos and linux.
-- [model](https://www.kaggle.com/models/google/movenet/frameworks/tfLite/variations/multipose-lightning-tflite-float16)
+- [model](https://www.kaggle.com/models/tensorflow/efficientdet/frameworks/tfLite)
+- [code example](https://github.com/finnickniu/tensorflow_object_detection_tflite/blob/master/demo.cpp)
--- main.cpp
+++ main.cpp
@@ -6,30 +6,29 @@
 
 using namespace std;
 using namespace cv;
-
-int main() {
-
-    namedWindow("Video Player");//Declaring the video to show the video//
-    auto* camera = new Camera(0);
-    auto* x = new ObjectDetectionModel();
-    x->setUpInterpreter(camera->getWidth(), camera->getHeight());
-    if (!camera->getVideoCaptureDevice().isOpened()){ //This section prompt an error message if no video stream is found//
-        cout << "No video stream detected" << endl;
-        system("pause");
-        return-1;
-    }
-    while (true){ //Taking an everlasting loop to show the video//
-        camera->captureFrame();
-        if (camera->getCurrentFrame()->empty()){ //Breaking the loop if no video frame is detected//
-            break;
-        }
-        // ObjectDetectionModel::processFrameInPlace(camera->getCurrentFrame());
-        imshow("Video Player", *(camera->getCurrentFrame()));
-        char c = (char)waitKey(25);//Allowing 25 milliseconds frame processing time and initiating break condition//
-        if (c == 27){ //If 'Esc' is entered break the loop//
-            break;
-        }
-    }
-    delete camera;
-    return 0;
-}
+//
+//int main() {
+//
+//    namedWindow("Video Player");//Declaring the video to show the video//
+//    auto* camera = new Camera(0);
+//    ObjectDetectionModel::setUpInterpreter(camera->getWidth(), camera->getHeight());
+//    if (!camera->getVideoCaptureDevice().isOpened()){ //This section prompt an error message if no video stream is found//
+//        cout << "No video stream detected" << endl;
+//        system("pause");
+//        return-1;
+//    }
+//    while (true){ //Taking an everlasting loop to show the video//
+//        camera->captureFrame();
+//        if (camera->getCurrentFrame()->empty()){ //Breaking the loop if no video frame is detected//
+//            break;
+//        }
+//        // ObjectDetectionModel::processFrameInPlace(camera->getCurrentFrame());
+//        imshow("Video Player", *(camera->getCurrentFrame()));
+//        char c = (char)waitKey(25);//Allowing 25 milliseconds frame processing time and initiating break condition//
+//        if (c == 27){ //If 'Esc' is entered break the loop//
+//            break;
+//        }
+//    }
+//    delete camera;
+//    return 0;
+//}
